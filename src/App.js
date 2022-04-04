570063f0a90af1a024f8c19b06003e8015eab448
Ankit-0212 <ankitrajsrivastava222@gmail.com>
2022-04-04 16:48:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React,{useEffect} from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import ScrollToTop from './components/ScrollToTop';
+import Services from './components/Services'
+import Recommend from './components/Recommend'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer';
+import scrollreveal from 'scrollreveal';
+export default function App() {
+  useEffect(()=>{
+    const sr=scrollreveal({
+      origin:"top",
+      distance:"80px",
+      duration: 2000,
+      reset: true
+    });
+    sr.reveal(
+      `nav,#hero,#services,#recommend,#testimonials,footer`,
+      {
+        opacity: 0,
+        interval: 300,
+      }
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    
+    <ScrollToTop/>
+    <Navbar/>
+    <Hero/>
+    <Services/>
+    <Recommend/>
+    <Testimonials/>
+    <Footer/>
+    </>
+  )
 }
-
-export default App;
